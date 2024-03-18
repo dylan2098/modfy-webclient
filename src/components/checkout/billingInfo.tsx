9a277b60709ff7502a1d8c1c0a2c46793fbcca27
Dylan <triminh.nguyen2098@gmail.com>
2024-03-18 17:41:40 +0700

    fix error

---
+++ src/components/checkout/billingInfo.tsx
@@ -0,0 +1,18 @@
+interface Props {
+  title?: string;
+}
+
+
+export default function OrderSummary({
+
+}: Props) {
+
+  return (
+    <>
+      <div className="form-check">
+        <input className="form-check-input" type="checkbox" value="" readOnly={false} defaultChecked={true}/>
+        <label className="custom-control-label">Same as shipping information</label>
+      </div>
+    </>
+  );
+}
