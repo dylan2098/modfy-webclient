cb14c21dc452f75d5374cdbd17e665f11d349266
Dylan <triminh.nguyen2098@gmail.com>
2024-03-21 13:57:55 +0700

    update invoice

--- src/components/order/orderSummaries.tsx
+++ src/components/order/orderSummaries.tsx
@@ -78,7 +78,106 @@ export default function OrderSummaries({
           <h3>Order #{order?.orderNumber}</h3>
           <p className="mb-0 text-dark mt-4 mt-md-0">Order Placed <b>{order?.date}</b></p>
         </div>
-        <button className="btn btn-dark btn-sm">View invoice</button>
+        <button className="btn btn-dark btn-sm mb-md-0 ms-4 md-md-0" data-bs-toggle="modal" data-bs-target="#invoiceModal1">View invoice</button>
+        <div className="modal fade" id="invoiceModal1" role="dialog" aria-labelledby="invoiceModal1" aria-hidden="true">
+            <div className="modal-dialog modal-dialog-centered modal-lg" role="document">
+              <div className="modal-content">
+                <div className="card p-4">
+                    <div className="card-header text-center">
+                      <div className="row justify-content-between">
+                        <div className="col-md-4 text-start">
+                          <img className="mb-2 w-50 rounded-2" src="https://s3.amazonaws.com/creativetim_bucket/new_logo.png" alt="Logo" />
+                          <h6>
+                            St. Independence Embankment, 050105 Bucharest, Romania
+                          </h6>
+                          <small className="d-block text-muted">tel: +4 (074) 1090873</small>
+                        </div>
+                        <div className="col-lg-4 col-md-5 text-end">
+                          <button type="button" className="btn-close float-end" data-bs-dismiss="modal" aria-label="Close"></button>
+                          <h4 className="mt-7 mb-1">Billed to:</h4>
+                          <span className="d-block">James Thompson</span>
+                          <p className="text-sm">
+                            Bld Mihail Kogalniceanu,<br />
+                            7652 Bucharest,<br />
+                            Romania
+                          </p>
+                        </div>
+                      </div>
+                      <div className="row justify-content-md-between">
+                        <div className="col-md-4">
+                          <h4 className="mt-5 text-start">Invoice no. <br /><small className="mr-2">#XK98321111</small></h4>
+                        </div>
+                        <div className="col-lg-4 col-md-5">
+                          <div className="d-flex align-items-center mt-5 ms-auto float-end">
+                            <div className="text-sm me-3 font-weight-bold">Invoice date:</div>
+                            <div className="text-end">Jan 22, 2022</div>
+                          </div>
+                          <div className="d-flex align-items-center ms-auto float-end">
+                            <div className="text-sm me-3 font-weight-bold">Due date:</div>
+                            <div className="text-end">Feb 22, 2022</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                <div className="card-body">
+                  <div className="row">
+                    <div className="col-12">
+                      <div className="table-responsive">
+                        <table className="table text-right">
+                          <thead className="bg-default">
+                            <tr>
+                              <th scope="col" className="text-right text-white">Item</th>
+                              <th scope="col" className="text-right text-white">Qty</th>
+                              <th scope="col" className="text-right text-white">Rate</th>
+                              <th scope="col" className="text-right text-white">Amount</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <td>Premium Support</td>
+                              <td>1</td>
+                              <td>$ 9.00</td>
+                              <td className="text-end">$ 9.00</td>
+                            </tr>
+                            <tr>
+                              <td>Argon Dashboard PRO</td>
+                              <td>3</td>
+                              <td>$ 100.00</td>
+                              <td className="text-end">$ 300.00</td>
+                            </tr>
+                            <tr>
+                              <td>Parts for service</td>
+                              <td>1</td>
+                              <td>$ 89.00</td>
+                              <td className="text-end">$ 89.00</td>
+                            </tr>
+                          </tbody>
+                          <tfoot>
+                            <tr>
+                              <th colSpan={3} className="h4">Total</th>
+                              <th className="text-end h4">$ 398.00</th>
+                            </tr>
+                          </tfoot>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="card-footer text-right">
+                  <div className="col-md-5 ms-auto text-end">
+                    <h5>Thank you!</h5>
+                    <p className="text-sm">If you encounter any issues related to the invoice you can contact us at:</p>
+                    <h6 className="mb-0">
+                      email:
+                      <small className="text-muted"> support@cretive-tim.com</small>
+                    </h6>
+                  </div>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
       </div>
 
       {orderCards}
